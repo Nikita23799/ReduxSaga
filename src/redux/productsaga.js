f77303a2977b7b30f9cb2e3f794ea4151d9c71bd
unknown <nikitapatil23799@gmail.com>
2022-09-04 10:55:48 +0530

    API call changes

--- src/redux/productsaga.js
+++ src/redux/productsaga.js
@@ -2,7 +2,7 @@ import { takeEvery, put } from 'redux-saga/effects'
 import { Product_List, Set_Product_List } from './constant';
 
 function* getProducts() {
-    let data = yield fetch('http://localhost:3004/products');
+    let data = yield fetch('http://localhost:3001/products');
     data = yield data.json();
     console.log("action is called", data)
     yield put({type: Set_Product_List, data})
